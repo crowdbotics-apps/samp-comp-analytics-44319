41013b140a20d4987d0dafa86cd55bebd1df4fbf
Crowdbotics <team@crowdbotics.com>
2023-11-03 17:35:30 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "samp-comp-analytics-44319",
+  url: "https://samp-comp-analytics-44319.botics.co",
+  api: "https://samp-comp-analytics-44319.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
